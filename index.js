2125740990d3d7333b2ff36f1ffe2b883907d825
jerry <brownjer3@gmail.com>
2021-02-03 16:08:01 -0800

    commit tree 1f5e15223b1ce7fa083da19d92712e913ed572d2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users"
+    let details = {
+        'name': name, 
+        'email': email
+    }
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(details)
+    }
+
+    return fetch(url, configObject)
+    .then(response => response.json())
+    .then(obj => {
+        document.body.innerHTML = obj["id"]
+    })
+    .catch(obj => {
+        document.body.innerHTML = obj.message
+    })
+}
+
